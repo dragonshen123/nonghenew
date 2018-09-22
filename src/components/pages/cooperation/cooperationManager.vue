538b37aec9a1b7d29fc7f6ffaad69c4df8852cff
shenlong123 <1603359663@qq.com>
2018-09-22 15:17:48 +0800

    Default

--- src/components/pages/cooperation/cooperationManager.vue
+++ src/components/pages/cooperation/cooperationManager.vue
@@ -9,31 +9,13 @@
       <div class="col col-md-3" id="d_menu">
         <div class="panel panel-primary">
           <div class="panel-heading">
-            <h3 class="panel-title">产品预售</h3>
+            <h3 class="panel-title">合作社信息</h3>
           </div>
           <div class="panel-body">
             <ul id="d_menu-list">
-              <li  v-on:click="getImformation(10,1,null)">全部分类<i
+              <li  v-on:click="getImformation(10,1,null)">合作社信息<i
                 class="pull-right icon iconfont icon-youjiantou"></i></li>
-              <li class="active" v-on:click="getImformation(10,1,'产品测量')">产品测量<i
-                class="pull-right icon iconfont icon-youjiantou"></i></li>
-              <li v-on:click="getImformation(10,1,'商品名称')">商品名称<i
-                class="pull-right icon iconfont icon-youjiantou"></i></li>
-              <li v-on:click="getImformation(10,1,'作物种类')">作物种类<i
-                class="pull-right icon iconfont icon-youjiantou"></i></li>
-              <li v-on:click="getImformation(10,1,'作物品种')">作物品种<i
-                class="pull-right icon iconfont icon-youjiantou"></i></li>
-              <li v-on:click="getImformation(10,1,'价格区间')">价格区间<i
-                class="pull-right icon iconfont icon-youjiantou"></i></li>
-              <li v-on:click="getImformation(10,1,'规格')">规格<i
-                class="pull-right icon iconfont icon-youjiantou"></i></li>
-              <li v-on:click="getImformation(10,1,'产品属性')">产品属性<i
-                class="pull-right icon iconfont icon-youjiantou"></i></li>
-              <li v-on:click="getImformation(10,1,'产品标准')">产品标准<i
-                class="pull-right icon iconfont icon-youjiantou"></i></li>
-              <li v-on:click="getImformation(10,1,'产品介绍')">产品介绍<i
-                class="pull-right icon iconfont icon-youjiantou"></i></li>
-              <li v-on:click="getImformation(10,1,'供应商信息')">供应商信息<i
+              <li class="active" v-on:click="getImformation(10,1,'产品测量')">地图展示<i
                 class="pull-right icon iconfont icon-youjiantou"></i></li>
 
             </ul>
@@ -46,14 +28,59 @@
 
             <form class="form-horizontal pull-right" style="width: 100%">
               <div class="form-group" style="margin-bottom: 0px">
-                <label class="col-sm-5 control-label" >产品预售名称</label>
-                <div class="col-sm-5">
-                  <input class="form-control" v-model="informationName" />
+                <label class="col-sm-1 control-label" >省</label>
+                <div class="col-sm-2">
+                  <select  class="form-control"  v-model="proviceValue" @change="getstate()" >
+                    <option  v-for="item in provices"  v-bind:value="item.locationId">{{item.locationName}}</option>
 
+                  </select>
+                </div>
+
+                <label  class="col-sm-1 control-label" style="width: 5%">州</label>
+                <div class="col-sm-2">
+                  <select type="text" class="form-control" v-model="statesValue" @change="getcounty()" >
+                    <option v-for="state in states" v-bind:value="state.locationId">{{state.locationName}}</option>
+                    <option ></option>
+                  </select>
+                </div>
+
+                <label  class="col-sm-1 control-label" style="width: 5%">县</label>
+                <div class="col-sm-2">
+                  <select type="text" class="form-control"  v-model="countyValue" @change="gettowns()">
+                    <option v-for="county in countys" v-bind:value="county.locationId">{{county.locationName}}</option>
+
+                  </select>
+                </div>
+
+                <label  class="col-sm-1 control-label" style="width: 5%">镇</label>
+                <div class="col-sm-2">
+                  <select type="text" class="form-control" v-model="townValue" @change="getvillages()" >
+                    <option v-for="town in towns" v-bind:value="town.locationId">{{town.locationName}}</option>
+
+                  </select>
                 </div>
 
 
-                <label class="col-sm-5 control-label" v-on:click="getImformation(pageSize,1,null)" >查询</label>
+                <label  class="col-sm-2 control-label" style="width: 9%">村委会</label>
+                <div class="col-sm-2">
+                  <select type="text" class="form-control" v-model="villageValue" @change="getgroups()">
+                    <option v-for="village in villages" v-bind:value="village.locationId">{{village.locationName}}</option>
+
+                  </select>
+                </div>
+
+                <label  class="col-sm-2 control-label" style="width: 9%">村小组</label>
+                <div class="col-sm-2">
+                  <select type="text" class="form-control" v-model="groupValue"  @change="getcencus()">
+                    <option v-for="group in groups" v-bind:value="group.groupId">{{group.groupName}}</option>
+                  </select>
+                </div>
+                <label  class="col-sm-2 control-label" style="width: 9%">合作社名称</label>
+                <div class="col-sm-2">
+                 <input name=" cooperationName" class="form-control" v-model=" cooperationName"/>
+                </div>
+
+                <label  class="col-sm-2 control-label"  v-on:click="queryPage(pageSize,1)">查询</label>
               </div>
             </form>
           </div>
@@ -66,17 +93,19 @@
           <table class="table table-striped" id="dataList">
             <thead>
             <tr>
-              <th>生产管理标题</th>
-              <th>生产管理类型</th>
-              <th>生产管理时间</th>
+              <th>合作社名称</th>
+              <th>党员数量</th>
+              <th>社员数</th>
+              <th>创建日期</th>
               <th>详情</th>
             </tr>
             </thead>
             <tbody>
             <tr v-for=" item in page">
-              <td>{{item.informationTtile}}</td>
-              <td>{{item.informationType}}</td>
-              <td>{{item.informationDate}}</td>
+              <td>{{item.cooperationName}}</td>
+              <td>{{item.cooperationPartyNumber}}</td>
+              <td>{{item.cooperationPersionNumber}}</td>
+              <td>{{item.cooperationDate}}</td>
               <td>跳转详情</td>
             </tr>
             </tbody>
@@ -103,39 +132,110 @@
   import VueResource from '@/components/resource/index.js'
   Vue.use(VueResource)
   export default {
-    name: "saleManager",
+    name: "cooperationManager",
     data: function () {
       return {
         showPage:[1,2,3,4,5],
-        informationName:null,
-        informationType:null,
-        lotValue:null,
+        provices:this.getLocation(0,0),
+        proviceValue:null,//省
+        states:null,//州
+        statesValue:null,
+        countys:null,//县
+        countyValue:null,
+        towns:null,//镇
+        townValue:null,
+        villages:null,//村委会
+        villageValue:null,
+        groups:null,//村小组
+        groupValue:null,
         totalCount:null,
         totalPage:null,
         pageSize:15,
         curPage:1,
-        page:null
+        page:null,
+        cooperationName:null
       }
     },
 
     created() {
-      this.getImformation(10,1,null)
+    //  this.getImformation(10,1,null)
+      this.queryPage(10,1)
     },
     methods: {
-   getImformation(pageSize,curPage,informationType){
-     this.curPage=curPage
-     this.informationType=informationType
-     var location = this.$resource(VueResource.data.url+'/webIndexController/asyncQueryPageproducts?pagenum='+this.curPage+'&pageSize='+pageSize+'&flag=2&informationType='+informationType+'&informationName='+this.informationName)
-     location.query().then(function (response) {
-       console.log(response.bodyText)
-       this.page= JSON.parse(response.bodyText).result
-       this.curPage=JSON.parse(response.bodyText).curPage
-       this.totalCount=JSON.parse(response.bodyText).totalCount
-       //this.pageSize=this.page.pageSize
-       this.totalPage=JSON.parse(response.bodyText).totalPage
-       console.log("当前页"+this.curPage)
-     })
-   }
+      getLocation:function(id,flag){
+        var location = this.$resource(VueResource.data.url+'/webIndexController/asyncGetNodes?id='+id)
+        location.query().then(function (response) {
+          console.log(response.bodyText)
+          if(flag==0){
+            this.provices=JSON.parse(response.bodyText)
+          }
+          if(flag==1){
+            this.states=JSON.parse(response.bodyText)
+          }
+          if(flag==2){
+            this.countys=JSON.parse(response.bodyText)
+          }
+          if(flag==3){
+            this.towns=JSON.parse(response.bodyText)
+          }
+          if(flag==4){
+            this.villages=JSON.parse(response.bodyText)
+          }
+          if(flag==5){
+            this.groups=JSON.parse(response.bodyText)
+          }
+        })
+      },
+      getstate:function(){
+        this.getLocation(this.proviceValue,1)
+      },
+      getcounty:function(){
+        this.getLocation(this.statesValue,2)
+      },
+      gettowns:function(){
+        this.getLocation(this.countyValue,3)
+      },
+      getvillages:function(){
+        this.getLocation(this.townValue,4)
+      },
+      getgroups:function(){
+        var location = this.$resource(VueResource.data.url+'/webIndexController/asyncGetgroup?locationCode='+this.villageValue)
+        location.query().then(function (response) {
+          console.log(response.bodyText)
+          this.groups= JSON.parse(response.bodyText)
+        })
+      },
+      getLocationCode:function(){
+        if(this.villageValue!=null){
+          return this.villageValue
+        }else if(this.townValue!=null){
+          return this.townValue
+        }else if(this.countyValue!=null){
+          return this.countyValue
+        }else if(this.statesValue!=null){
+          return this.statesValue
+        }else if(this.proviceValue!=null){
+          return this.proviceValue
+        }
+      },
+      queryPage:function(pageSize,curPage){
+        var queryPage = this.$resource(VueResource.data.url+'/webIndexController/asyncQueryPageCooperation?' +
+          'code='+ this.getLocationCode()+
+          '&pageSize='+ pageSize+
+          '&curPage='+ curPage+
+          '&groupValue='+ this.groupValue+
+          '&cooperationName='+ this.cooperationName
+        )
+        queryPage.query().then(function (response) {
+          console.log(response.bodyText)
+          this.page= JSON.parse(response.bodyText).result
+          this.curPage=JSON.parse(response.bodyText).curPage
+          this.totalCount=JSON.parse(response.bodyText).totalCount
+          //this.pageSize=this.page.pageSize
+          this.totalPage=JSON.parse(response.bodyText).totalPage
+          console.log("当前页"+this.curPage)
+        })
+      }
     }
   }
 
